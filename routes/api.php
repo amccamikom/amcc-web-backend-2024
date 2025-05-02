2e5d2fefb212340d60185ddbf84c1b035e214586
hansfigo <hambasahaya0303@gmail.com>
2025-05-03 00:49:23 +0700

    fix: update UserController namespace in API routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\CarController;
+use App\Http\Controllers\UserController;
 use App\Http\Middleware\ApiKeyMiddleware;
 use Illuminate\Support\Facades\Route;
 
