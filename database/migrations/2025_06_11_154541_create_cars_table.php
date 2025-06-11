f4b167c6cc74a871c510bdd6db83e9efd8695a6d
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2025-06-11 23:07:34 +0700

    feat: add car and booking models and migrations
    - Added `Car` model with attributes like name, color, price, speed, seats, location, rating, and availability.
    - Added `Booking` model to manage car bookings, including car ID, duration, booking date, and user information.
    - Created migrations for `cars` and `bookings` tables with appropriate fields and constraints.
    - Established relationships between `Car` and `Booking` models (one-to-many).

---
+++ database/migrations/2025_06_11_154541_create_cars_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('color');
+            $table->unsignedInteger('price');
+            $table->unsignedInteger('speed');
+            $table->unsignedTinyInteger('seats');
+            $table->string('location');
+            $table->decimal('rating', 2, 1)->nullable();
+            $table->boolean('is_available')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
